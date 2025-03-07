b2346938be1e4dc2844b6b0a524fe3575dc49335
anetafabijanska <anetafabijanska@gmail.com>
2025-03-07 18:15:52 +0000

    again 4a

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y sl
 
-echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
+echo "export PATH=$PATH:/usr/games" >> ~/.bashrc
 source ~/.bashrc
 
 # Uruchomienie komendy sl
