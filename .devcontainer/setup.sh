f53c7f47165bd4361e1875be35fc50d076130bd6
anetafabijanska <anetafabijanska@gmail.com>
2025-03-07 01:55:14 +0000

    step 4b

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,5 +2,10 @@
 
 sudo apt-get update
 sudo apt-get install -y sl
+
 echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
-source ~/.bashrc 
+source ~/.bashrc
+
+# Debugging information
+echo "PATH after sourcing .bashrc: $PATH"
+which sl
